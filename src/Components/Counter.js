c2d458f24d3b9c63602d7021d643716f2ce636b3
0sergej <sergej2stojanovic@gmail.com>
2021-11-13 20:30:00 +0100

    this.setState

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component
 {
+
+    constructor(props)
+    {
+        super(props)
+
+        this.state = {
+            count: props.initailCount
+        }
+    }
+    
     render()
     {
         return (<div>
-    <button className="btn">-</button>
-    <span>0</span>
-    <button>+</button>
+    <button onClick={() => this.changeCount(-1)}>-</button>
+            <span>{this.state.count}</span>
+    <button onClick={() => this.changeCount(1)}>+</button>
   </div> )
+    }
+
+    changeCount(amount)
+    {
+        this.setState(prevState =>
+        {
+            return {count: prevState.count + amount}
+        })
     }
 }
