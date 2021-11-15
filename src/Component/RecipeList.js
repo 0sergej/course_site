8acce7613ee93937e9284eec6b822602b9f561fd
0sergej <sergej2stojanovic@gmail.com>
2021-11-15 19:52:45 +0100

    site v2

---
+++ src/Component/RecipeList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Recipe from './Recipe'
+
+export default function RecipeList({ recipes })
+{
+    return (
+        <div>
+            {
+                recipes.map(recipes =>
+                {
+                    return (
+                        <Recipe
+                            key={recipes.id} 
+                            {...recipes} />
+                    )
+                })}
+        </div>
+    )
+}
