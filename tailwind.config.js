27bbec23cbb7f87df47a2bc67d868aa78b748a97
0sergej <sergej2stojanovic@gmail.com>
2021-11-16 21:17:18 +0100

    tailwindCSS default colors fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
+const {colors: {...colors}} = require('tailwindcss/defaultTheme')
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
+      colors,
       blue: {
         light: '#0B5579',
         dark: '#0077b3'
       },
-      white: '#fff',
       red: {
         light: '#ff1800',
         dark: '#b31200'
-      }
+      },
     }
   },
   variants: {
